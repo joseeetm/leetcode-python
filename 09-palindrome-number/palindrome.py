a298fc0b862023a6931cbb202f8bef708e50305c
Jose Tirado <jtirado@civitatis.com>
2026-01-12 17:32:01 +0100

    Palindrome exercise

--- 09-palindrome-number/palindrome.py
+++ 09-palindrome-number/palindrome.py
@@ -1,15 +1,15 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
+        if x >= 0:
+            reversed_num = ""
+            for num in str(x)[::-1]:
+                reversed_num += num
 
-        reversed_num = ""
-        for num in str(x)[::-1]:
-            reversed_num += num
+            if x == int(reversed_num):
+                return True
+            else:
+                return False
+        else: 
+                return False
 
-        if x == int(reversed_num):
-            return True
-        else:
-            return False
-
-print(Solution().isPalindrome(121)) 
+print(Solution().isPalindrome(-101)) 
